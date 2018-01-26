97cd9a242cfd234ff597108b6937f326ecca8976
Rowde <35587087+Rowde@users.noreply.github.com>
2018-01-25 22:04:59 -0600

    Add files via upload

---
+++ src/assignments/main_assignment2.py
@@ -0,0 +1,4 @@
+from assignment2 import faculty_evaluation_result
+
+'''Write code to call the faculty_evaluation_result function with data of your choice'''
+print(faculty_evaluation_result(0,0,67,88,35,123))
