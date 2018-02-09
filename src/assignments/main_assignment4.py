af7657c208b5d7d2f11ea5ee1281b7dc303e7905
Rowde <35587087+Rowde@users.noreply.github.com>
2018-02-08 22:10:26 -0600

    Add files via upload

---
+++ src/assignments/main_assignment4.py
@@ -0,0 +1,37 @@
+import assignment4
+
+def main():#void function
+    '''
+    Create a loop that'll run until the user doesn't type the letter 'y'
+    In the loop,
+    Capture one number from the keyboard and validate for a range between 1 and 10.
+    Call the factorial function.
+    Save the result to a variable.
+    Print the variable value.
+
+    Ask the user if they want to continue.
+
+    TO RUN YOUR PROGRAM GO TO IN PYTHON IDLE RUN->RUN MODULE.
+    IN THE PYTHON SHELL TYPE main()
+
+    DON'T ADD A RETURN STATEMENT TO THIS FUNCTION
+    '''
+    keep_going = 'y'
+
+    while keep_going == 'y':
+        number = int(input('Please enter a number between 1 and 10: '))
+
+        if number < 1 or number > 10:
+            print('Invalid number')
+            number = int(input('Please enter a number between 1 and 10: '))
+
+        else:
+            result = assignment4.factorial(number)
+
+        print('The factorial of ',number,' is ',result,'.')
+        print()
+        keep_going = str(input('Type y to perform another factorial: '))
+                              
+
+#another option to run the program is to call the main() function below and Run->Run Module
+main()
